a9e862c33890a403b9351713bc104f0b8fe44891
Patrick Gaskin <patrick@pgaskin.net>
2025-06-08 20:16:45 -0400

    Generate js schema

--- schema/schema.go
+++ schema/schema.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-//go:generate go run github.com/bufbuild/buf/cmd/buf@v1.50.1 generate --template {"version":"v2","plugins":[{"local":["go","tool","protoc-gen-go"],"out":".","opt":["paths=source_relative","Mschema.proto=./schema"]}]}
+//go:generate go run github.com/bufbuild/buf/cmd/buf@v1.50.1 generate --template {"version":"v2","plugins":[{"local":["go","tool","protoc-gen-go"],"out":".","opt":["paths=source_relative","Mschema.proto=./schema"]},{"local":["npm","exec","@bufbuild/protoc-gen-es@v2.5.2"],"out":".","opt":["target=js+dts"]}]}
 
 func ToWeekday(w time.Weekday) Weekday {
 	return Weekday(w)
