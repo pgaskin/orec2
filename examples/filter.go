737f20833dbf6a3d3557dca1c8de2e1c7c5ef828
Patrick Gaskin <patrick@pgaskin.net>
2025-09-01 21:26:50 -0400

    Improve filter example

--- examples/filter.go
+++ examples/filter.go
@@ -14,22 +14,61 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-var tmpl = template.Must(template.New("").Funcs(template.FuncMap{
-	"ansi": func(i ...int) string {
-		if len(i) == 0 {
-			return "\x1b[0m"
+func main() {
+	name := "data/data.pb"
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	var data schema.Data
+	if buf, err := os.ReadFile(name); err != nil {
+		panic(err)
+	} else if err := proto.Unmarshal(buf, &data); err != nil {
+		panic(err)
+	}
+	filter(&data, func(fac *schema.Facility, sg *schema.ScheduleGroup, sch *schema.Schedule, act *schema.Schedule_Activity, day *schema.Schedule_ActivityDay, tr *schema.TimeRange) bool {
+		switch fac.GetName() {
+		case "CARDELREC Recreation Complex Goulbourn",
+			"Kanata Leisure Centre and Wave Pool",
+			"Minto Recreation Complex - Barrhaven",
+			"Richcraft Recreation Complex-Kanata",
+			"Tony Graham Recreation Complex - Kanata":
+			return false // exclude these facilities
 		}
-		var b strings.Builder
-		b.WriteString("\x1b[")
-		for x, y := range i {
-			if x != 0 {
-				b.WriteByte(';')
+
+		if name := act.GetXName(); name != "" {
+			switch {
+			case strings.Contains(name, "adult skate"):
+			case strings.Contains(name, "family skate"):
+			case strings.Contains(name, "public skate"):
+			//case strings.Contains(name, "lane swim"):
+			default:
+				return false // exclude other activities
 			}
-			b.WriteString(strconv.Itoa(y))
 		}
-		b.WriteByte('m')
-		return b.String()
-	},
+
+		wd, cr, ok := tr2cr(tr)
+		if !ok {
+			return false // cannot filter
+		}
+		var want []schema.ClockRange
+		switch wd {
+		case time.Saturday, time.Sunday:
+			want = append(want, schema.MakeClockRange(12, 00, 23, 00))
+		default:
+			want = append(want, schema.MakeClockRange(6, 00, 9, 00))
+			want = append(want, schema.MakeClockRange(18, 00, 23, 00))
+		}
+		return slices.ContainsFunc(want, cr.Overlaps) // exclude if doesn't overlap a time we want
+	})
+	if err := tmpl.Execute(os.Stdout, &data); err != nil {
+		panic(err)
+	}
+}
+
+// tmpl is a template for rendering recreation schedules as text. It does not
+// include information about schedule exceptions or scrape errors.
+var tmpl = template.Must(template.New("").Funcs(template.FuncMap{
+	"ansi": ansi,
 }).Parse(`
 {{- range $f := .GetFacilities }}
 {{- range $g := .GetScheduleGroups }}
@@ -52,62 +91,51 @@ var tmpl = template.Must(template.New("").Funcs(template.FuncMap{
 {{- end }}
 `))
 
-func main() {
-	var data schema.Data
-	if buf, err := os.ReadFile("data/data.pb"); err != nil {
-		panic(err)
-	} else if err := proto.Unmarshal(buf, &data); err != nil {
-		panic(err)
+// ansi formats an ansi escape sequence.
+func ansi(i ...int) string {
+	if len(i) == 0 {
+		return "\x1b[0m"
 	}
-	data.SetFacilities(slices.DeleteFunc(data.GetFacilities(), func(facility *schema.Facility) bool {
-		switch facility.GetName() {
-		case "CARDELREC Recreation Complex Goulbourn",
-			"Kanata Leisure Centre and Wave Pool",
-			"Minto Recreation Complex - Barrhaven",
-			"Richcraft Recreation Complex-Kanata",
-			"Tony Graham Recreation Complex - Kanata":
-			return true // exclude
+	var b strings.Builder
+	b.WriteString("\x1b[")
+	for x, y := range i {
+		if x != 0 {
+			b.WriteByte(';')
 		}
+		b.WriteString(strconv.Itoa(y))
+	}
+	b.WriteByte('m')
+	return b.String()
+}
+
+// filter filters activity times with the specified function, removing any empty
+// activities, schedules, schedule groups, or facilities.
+func filter(data *schema.Data, fn func(*schema.Facility, *schema.ScheduleGroup, *schema.Schedule, *schema.Schedule_Activity, *schema.Schedule_ActivityDay, *schema.TimeRange) bool) {
+	data.SetFacilities(slices.DeleteFunc(data.GetFacilities(), func(facility *schema.Facility) bool {
 		facility.SetScheduleGroups(slices.DeleteFunc(facility.GetScheduleGroups(), func(group *schema.ScheduleGroup) bool {
 			group.SetSchedules(slices.DeleteFunc(group.GetSchedules(), func(schedule *schema.Schedule) bool {
 				schedule.SetActivities(slices.DeleteFunc(schedule.GetActivities(), func(activity *schema.Schedule_Activity) bool {
-					switch {
-					case strings.Contains(activity.GetXName(), "adult skate"):
-					case strings.Contains(activity.GetXName(), "family skate"):
-					case strings.Contains(activity.GetXName(), "public skate"):
-					//case strings.Contains(activity.XName, "lane swim"):
-					default:
-						return true // include
-					}
 					for _, day := range activity.GetDays() {
-						day.SetTimes(slices.DeleteFunc(day.GetTimes(), func(tr *schema.TimeRange) bool {
-							if !tr.HasXWkday() || !tr.HasXStart() || !tr.HasXEnd() {
-								return false // cannot filter
-							}
-							var tvals []schema.ClockRange
-							switch time.Weekday(tr.GetXWkday()) {
-							case time.Saturday, time.Sunday:
-								tvals = append(tvals, schema.MakeClockRange(12, 00, 23, 00))
-							default:
-								tvals = append(tvals, schema.MakeClockRange(6, 00, 9, 00))
-								tvals = append(tvals, schema.MakeClockRange(18, 00, 23, 00))
-							}
-							return !slices.ContainsFunc(tvals, func(v schema.ClockRange) bool {
-								return schema.ClockRange{Start: schema.ClockTime(tr.GetXStart()), End: schema.ClockTime(tr.GetXEnd())}.Overlaps(v) // exclude if doesn't overlap
-							})
+						day.SetTimes(slices.DeleteFunc(day.GetTimes(), func(time *schema.TimeRange) bool {
+							return !fn(facility, group, schedule, activity, day, time)
 						}))
 					}
 					return !slices.ContainsFunc(activity.GetDays(), func(day *schema.Schedule_ActivityDay) bool {
-						return len(day.GetTimes()) != 0 // exclude if empty
+						return len(day.GetTimes()) != 0
 					})
 				}))
-				return len(schedule.GetActivities()) == 0 // exclude if empty
+				return len(schedule.GetActivities()) == 0
 			}))
-			return len(group.GetSchedules()) == 0 // exclude if empty
+			return len(group.GetSchedules()) == 0
 		}))
-		return len(facility.GetScheduleGroups()) == 0 // exclude if empty
+		return len(facility.GetScheduleGroups()) == 0
 	}))
-	if err := tmpl.Execute(os.Stderr, &data); err != nil {
-		panic(err)
+}
+
+// tr2cr gets the parsed time range, if parseable.
+func tr2cr(tr *schema.TimeRange) (time.Weekday, schema.ClockRange, bool) {
+	if !tr.HasXWkday() || !tr.HasXStart() || !tr.HasXEnd() {
+		return 0, schema.ClockRange{}, false
 	}
+	return time.Weekday(tr.GetXWkday()), schema.ClockRange{Start: schema.ClockTime(tr.GetXStart()), End: schema.ClockTime(tr.GetXEnd())}, true
 }
