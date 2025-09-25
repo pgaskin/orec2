pgaskin/orec2

commit 6843135c30677559adaed049907f792d1ce1ac76

Refactor http stuff, improve cache structure

scraper/main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"cmp"
 	"context"
-	"crypto/sha1"
-	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"flag"
@@ -29,6 +25,7 @@ import (
 	"unicode"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/pgaskin/orec2/internal/httpcache"
 	"github.com/pgaskin/orec2/internal/zyte"
 	"github.com/pgaskin/orec2/schema"
 	"golang.org/x/text/cases"
@@ -75,17 +72,40 @@ func defaultUserAgent() string {
 	return ua.String()
 }
 
-var geocodioLimit = sync.OnceValue(func() *rate.Limiter {
-	return rate.NewLimiter(rate.Every(time.Minute/1000), 1)
-})
+func main() {
+	flag.Parse()
 
-var pageLimit = sync.OnceValue(func() *rate.Limiter {
-	return rate.NewLimiter(rate.Limit(*PageQPS), 1)
-})
+	if b, _ := strconv.ParseBool(os.Getenv("OREC2_DEBUG_HTTP")); b {
+		next := http.DefaultTransport
+		http.DefaultTransport = roundTripperFunc(func(r *http.Request) (*http.Response, error) {
+			fmt.Println()
+			fmt.Println()
+			fmt.Println()
+			buf, err := httputil.DumpRequestOut(r, true)
+			if err != nil {
+				return nil, err
+			}
+			os.Stdout.Write(buf)
+			fmt.Println()
+			resp, err := next.RoundTrip(r)
+			if err != nil {
+				return nil, err
+			}
+			buf, err = httputil.DumpResponse(resp, true)
+			if err != nil {
+				return nil, err
+			}
+			os.Stdout.Write(buf)
+			fmt.Println()
+			fmt.Println()
+			fmt.Println()
+			return resp, nil
+		})
+	}
 
-var zyteClient = sync.OnceValue(func() *http.Client {
-	return &http.Client{
-		Transport: &zyte.Transport{
+	// use zyte for some requests
+	if *Zyte {
+		next := &zyte.Transport{
 			APIKey: ZyteAPIKey,
 			Limit:  zyte.FixedLimit(*ZyteLimit),
 			Retry: func(ctx context.Context, tries, code int) bool {
@@ -96,23 +116,56 @@ var zyteClient = sync.OnceValue(func() *http.Client {
 				return true
 			},
 			FollowRedirect: true,
-		},
+			Next:           http.DefaultTransport,
+		}
+		http.DefaultTransport = roundTripperFunc(func(r *http.Request) (*http.Response, error) {
+			if matchDomain(".ottawa.ca", r.URL) {
+				r2 := *r
+				r2.Header = r.Header.Clone()
+				r2.Header.Del("User-Agent")
+				r2.Header.Del("Cookie")
+				r = &r2
+				return next.RoundTrip(r)
+			}
+			return next.Next.RoundTrip(r)
+		})
 	}
-})
 
-func init() {
+	// apply rate limits if not cached
+	http.DefaultTransport = rateLimitRoundTripper(http.DefaultTransport, ".ottawa.ca", rate.NewLimiter(rate.Limit(*PageQPS), 1))
+	http.DefaultTransport = rateLimitRoundTripper(http.DefaultTransport, "api.geocod.io", rate.NewLimiter(rate.Every(time.Minute/1000), 1))
+
+	// cache responses
+	redactor := new(httpcache.Redactor)
+	cache := &httpcache.Transport{
+		Path:             *CacheDir,
+		Fallback:         *NoFetch, // for backwards compat with old caches
+		RequestRedactor:  redactor,
+		ResponseRedactor: redactor,
+	}
+	if !*NoFetch {
+		cache.Next = http.DefaultTransport
+	}
+	http.DefaultTransport = cache
+
+	// add secrets
 	if ScraperSecret != "" {
-		http.DefaultTransport = redactedHeaderRoundTripper(http.DefaultTransport, ".ottawa.ca", "X-Scraper-Secret", "", ScraperSecret)
+		http.DefaultTransport = headerRoundTripper(http.DefaultTransport, ".ottawa.ca", "X-Scraper-Secret", ScraperSecret)
+		redactor.RedactRequestHeader("X-Scraper-Secret", 4)
 	}
 	if GeocodioAPIKey != "" {
-		http.DefaultTransport = redactedHeaderRoundTripper(http.DefaultTransport, "api.geocod.io", "Authorization", "Bearer ", GeocodioAPIKey)
+		http.DefaultTransport = headerRoundTripper(http.DefaultTransport, "api.geocod.io", "Authorization", "Bearer "+GeocodioAPIKey)
+		redactor.RedactRequestHeader("Authorization", 4)
 	}
+
+	// add user agent
+	if ua := defaultUserAgent(); ua != "" {
+		http.DefaultTransport = headerRoundTripper(http.DefaultTransport, "", "User-Agent", ua)
+	}
+
+	// set up the default http client
 	http.DefaultClient.Transport = http.DefaultTransport
 	http.DefaultClient.Jar, _ = cookiejar.New(nil)
-}
-
-func main() {
-	flag.Parse()
 
 	if err := run(context.Background()); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
@@ -157,7 +210,7 @@ func run(ctx context.Context) error {
 		facilities int
 	)
 	for cur != "" {
-		doc, _, err := fetchPage(ctx, cur)
+		doc, _, err := fetchPage(ctx, "listing", cur)
 		if err != nil {
 			return err
 		}
@@ -196,7 +249,7 @@ func run(ctx context.Context) error {
 				}
 			}
 
-			doc, date, err := fetchPage(ctx, u.String())
+			doc, date, err := fetchPage(ctx, "facility", u.String())
 			if err != nil {
 				slog.Warn("failed to fetch place", "name", name, "error", err)
 				facility.XErrors = append(facility.XErrors, fmt.Sprintf("failed to fetch data: %v", err))
@@ -441,13 +494,18 @@ func run(ctx context.Context) error {
 //   - Pelias is better than Geocodio at choosing a point near the entrance instead of somewhere on the property.
 //   - For incorrect street names, Geocodio is better at resolving them based on the postal code, but Pelias just ignores the street and chooses somewhere seemingly random.
 func geocode(ctx context.Context, addr string) (lng, lat float64, attrib string, ok bool, err error) {
-	resp, err := fetchGeocodio(ctx, &url.URL{
-		Path: "v1.9/geocode",
+	u := &url.URL{
+		Scheme: "https",
+		Host:   "api.geocod.io",
+		Path:   "/v1.9/geocode",
 		RawQuery: url.Values{
 			"q":       {addr},
 			"country": {"CA"},
 		}.Encode(),
-	})
+	}
+	slog.Info("fetch geocodio", "url", u.String())
+
+	resp, err := fetch(ctx, "geocode", u.String())
 	if err != nil {
 		return 0, 0, "", false, err
 	}
@@ -485,10 +543,10 @@ func geocode(ctx context.Context, addr string) (lng, lat float64, attrib string,
 	return 0, 0, "", false, nil
 }
 
-func fetchPage(ctx context.Context, u string) (*goquery.Document, time.Time, error) {
-	slog.Info("fetch page", "url", u)
+func fetchPage(ctx context.Context, category, u string) (*goquery.Document, time.Time, error) {
+	slog.Info("fetch page", "url", u, "category", category)
 
-	resp, err := fetch(ctx, u, "page", u, pageLimit(), true)
+	resp, err := fetch(ctx, category, u)
 	if err != nil {
 		return nil, time.Time{}, err
 	}
@@ -511,88 +569,14 @@ func fetchPage(ctx context.Context, u string) (*goquery.Document, time.Time, err
 	return doc, date, nil
 }
 
-func fetchGeocodio(ctx context.Context, r *url.URL) (*http.Response, error) {
-	slog.Info("fetch geocodio", "url", r.String())
-
-	u, err := url.Parse("https://api.geocod.io/")
+func fetch(ctx context.Context, category, u string) (*http.Response, error) {
+	req, err := http.NewRequestWithContext(httpcache.CategoryContext(ctx, category), http.MethodGet, u, nil)
 	if err != nil {
 		return nil, err
 	}
-	q := u.Query()
-	u = u.ResolveReference(r)
-	maps.Copy(q, r.Query()) // keep orig query params
-	u.RawQuery = q.Encode()
-
-	return fetch(ctx, u.String(), "geocodio", r.String(), geocodioLimit(), false)
-}
-
-func fetch(ctx context.Context, u string, cacheType, cacheKey string, limiter *rate.Limiter, proxy bool) (*http.Response, error) {
-	var cacheName string
-	if *CacheDir != "" {
-		s := sha1.Sum([]byte(cacheKey))
-		cacheName = filepath.Join(*CacheDir, cacheType+"-"+hex.EncodeToString(s[:]))
-	}
-
-	var resp *http.Response
-	if cacheName != "" {
-		if buf, err := os.ReadFile(cacheName); err == nil {
-			r := bufio.NewReader(bytes.NewReader(buf))
-
-			req, err := http.ReadRequest(r)
-			if err != nil {
-				return nil, fmt.Errorf("read cached response: %w", err)
-			}
-			req.URL.Scheme = "https"
-			req.URL.Host = req.Host
-
-			resp, err = http.ReadResponse(r, req)
-			if err != nil {
-				return nil, fmt.Errorf("read cached response: %w", err)
-			}
-		} else if !errors.Is(err, os.ErrNotExist) {
-			return nil, fmt.Errorf("read cached response: %w", err)
-		} else if *NoFetch {
-			return nil, fmt.Errorf("get %q: fetch disabled, response not in cache", u)
-		}
-	}
-	if resp == nil {
-		req, err := http.NewRequestWithContext(ctx, http.MethodGet, u, nil)
-		if err != nil {
-			return nil, err
-		}
-
-		if limiter != nil {
-			if err := limiter.Wait(ctx); err != nil {
-				return nil, err
-			}
-		}
-
-		if proxy && *Zyte {
-			resp, err = zyteClient().Do(req)
-		} else {
-			req.Header.Set("User-Agent", *UserAgent)
-			resp, err = http.DefaultClient.Do(req)
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		if cacheName != "" {
-			defer resp.Body.Close()
-
-			reqbuf, err := httputil.DumpRequest(resp.Request, true)
-			if err != nil {
-				return nil, err
-			}
-
-			respbuf, err := httputil.DumpResponse(resp, true)
-			if err != nil {
-				return nil, err
-			}
-			if err := os.WriteFile(cacheName, slices.Concat(reqbuf, respbuf), 0666); err != nil {
-				return nil, fmt.Errorf("write cached response: %w", err)
-			}
-		}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
 	}
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("response status %d", resp.StatusCode)
@@ -1359,25 +1343,44 @@ func (fn roundTripperFunc) RoundTrip(r *http.Request) (*http.Response, error) {
 
 var _ http.RoundTripper = roundTripperFunc(nil)
 
-func redactedHeaderRoundTripper(next http.RoundTripper, domain, header, prefix, value string) http.RoundTripper {
-	sha := sha1.Sum([]byte(value))
+func headerRoundTripper(next http.RoundTripper, domain, name, value string) http.RoundTripper {
 	return roundTripperFunc(func(r *http.Request) (*http.Response, error) {
-		if h, d := strings.Trim(strings.ToLower(r.URL.Hostname()), "."), strings.ToLower(domain); !(d == "" || h == d || (d[0] == '.' && h == d[1:]) || (d[0] == '.' && strings.HasSuffix(h, d))) {
-			return next.RoundTrip(r)
+		if matchDomain(domain, r.URL) {
+			r2 := *r
+			r2.Header = r.Header.Clone()
+			if value == "" {
+				r2.Header.Del(name)
+			} else {
+				r2.Header.Set(name, value)
+			}
+			r = &r2
 		}
+		return cmp.Or(next, http.DefaultTransport).RoundTrip(r)
+	})
+}
 
-		r2 := *r
-		r = &r2
-		r.Header = maps.Clone(r.Header)
-		r.Header[header] = []string{prefix + value}
-
-		resp, err := next.RoundTrip(r)
-
-		if resp != nil && resp.Request != nil && resp.Request.Header != nil {
-			if _, ok := resp.Request.Header[header]; ok {
-				resp.Request.Header[header] = []string{prefix + "redacted-" + hex.EncodeToString(sha[:4])}
+func rateLimitRoundTripper(next http.RoundTripper, domain string, limiter *rate.Limiter) http.RoundTripper {
+	return roundTripperFunc(func(r *http.Request) (*http.Response, error) {
+		if matchDomain(domain, r.URL) {
+			if err := limiter.Wait(r.Context()); err != nil {
+				return nil, err
 			}
 		}
-		return resp, err
+		return cmp.Or(next, http.DefaultTransport).RoundTrip(r)
 	})
+}
+
+func matchDomain(domain string, u *url.URL) bool {
+	if domain == "" {
+		return true // match all
+	}
+	h := strings.Trim(strings.ToLower(u.Hostname()), ".")
+	d := strings.ToLower(domain)
+	if h == d {
+		return true // exact match
+	}
+	if d[0] == '.' {
+		return h == d[1:] || strings.HasSuffix(h, d)
+	}
+	return false
 }

.github/workflows/ci.yml
@@ -13,6 +13,9 @@ on:
       keep_facility_list:
         required: false
         type: boolean
+      keep_geocoding:
+        required: false
+        type: boolean
 
 permissions:
   contents: write
@@ -46,10 +49,12 @@ jobs:
           go-version-file: go.mod
       - run: git config --global user.name github-actions && git config --global user.email github-actions@github.com
       - run: git worktree add cache cache
-      - run: grep -m1 'GET ' cache/page-* | grep -v 'place-listing[^/]' | cut -d':' -f1 | xargs rm
-        if: ${{ github.event_name != 'push' }} # we only want to clear the cached data if it's a scheduled or forced update... otherwise, just download anything new if needed
-      - run: rm -fv cache/page-*
-        if: ${{ github.event_name != 'push' && !(github.event.inputs != null && (github.event.inputs.keep_facility_list == true || github.event.inputs.keep_facility_list == 'true')) }} # we only want to clear the cached data if it's a scheduled or forced update... otherwise, just download anything new if needed
+      - run: rm -fv cache/facility-*
+        if: ${{ github.event_name != 'push' }}
+      - run: rm -fv cache/listing-*
+        if: ${{ github.event_name != 'push' && !(github.event.inputs != null && (github.event.inputs.keep_facility_list == true || github.event.inputs.keep_facility_list == 'true')) }}
+      - run: rm -fv cache/geocode-*
+        if: ${{ github.event_name != 'push' && github.event_name != 'schedule' && !(github.event.inputs != null && (github.event.inputs.keep_geocoding == true || github.event.inputs.keep_geocoding == 'true')) }}
       - run: go run ./scraper -cache-dir ./cache -geocodio ${{ github.event.inputs != null && (github.event.inputs.zyte == true || github.event.inputs.zyte == 'true') && '-zyte' || '' }}
         env:
           ZYTE_APIKEY: ${{ secrets.zyte_apikey }}

internal/httpcache/httpcache.go
@@ -1,0 +1,235 @@
+package httpcache
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"crypto/sha1"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io/fs"
+	"net/http"
+	"net/http/httputil"
+	"net/textproto"
+	"net/url"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+)
+
+// Transport caches HTTP responses indefinitely based on a URL and an optional
+// category. It supports redacting sensitive headers/parameters.
+type Transport struct {
+	// Path is the path to store cached requests at.
+	Path string
+
+	// Fallback allows pages from any category to be used.
+	Fallback bool
+
+	// RequestRedactor redacts requests for storage.
+	RequestRedactor RequestRedactor
+
+	// ResponseRedactor redacts responses.
+	ResponseRedactor ResponseRedactor
+
+	// Next is the transport to use for making requests. If nil, only cached
+	// responses are used.
+	Next http.RoundTripper
+}
+
+type categoryKey struct{}
+
+func CategoryContext(ctx context.Context, category string) context.Context {
+	return context.WithValue(ctx, categoryKey{}, category)
+}
+
+func WithCategory(r *http.Request, category string) *http.Request {
+	return r.WithContext(CategoryContext(r.Context(), category))
+}
+
+func contextCategory(ctx context.Context) string {
+	if v, ok := ctx.Value(categoryKey{}).(string); ok && v != "" {
+		return v
+	}
+	return "req"
+}
+
+func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	if req.Method != http.MethodGet {
+		return nil, fmt.Errorf("httpcache: unsupported method %s", req.Method)
+	}
+
+	var cacheName, cacheSuffix string
+	if t.Path != "" {
+		s := sha1.Sum([]byte(req.URL.String()))
+		cacheSuffix = "-" + hex.EncodeToString(s[:])
+		cacheName = filepath.Join(t.Path, contextCategory(req.Context())+cacheSuffix)
+	}
+
+	var resp *http.Response
+	if cacheName != "" {
+		buf, err := os.ReadFile(cacheName)
+		if t.Fallback && errors.Is(err, fs.ErrNotExist) {
+			ds, err1 := os.ReadDir(t.Path)
+			if err1 != nil {
+				return nil, fmt.Errorf("httpcache: scan cache: %w", err)
+			}
+			for _, d := range ds {
+				if strings.HasSuffix(d.Name(), cacheSuffix) {
+					buf, err = os.ReadFile(filepath.Join(t.Path, d.Name()))
+					if err != nil {
+						return nil, fmt.Errorf("httpcache: read fallback cached response: %w", err)
+					}
+					break
+				}
+			}
+		}
+		if err == nil {
+			fmt.Println(cacheName)
+			r := bufio.NewReader(bytes.NewReader(buf))
+
+			req, err := http.ReadRequest(r)
+			if err != nil {
+				return nil, fmt.Errorf("httpcache: read cached response: %w", err)
+			}
+			req.URL.Scheme = "https"
+			req.URL.Host = req.Host
+
+			resp, err = http.ReadResponse(r, req)
+			if err != nil {
+				return nil, fmt.Errorf("httpcache: read cached response: %w", err)
+			}
+			return resp, nil
+		} else if !errors.Is(err, os.ErrNotExist) {
+			return nil, fmt.Errorf("httpcache: read cached response: %w", err)
+		}
+	}
+
+	if t.Next == nil {
+		if cacheName == "" {
+			return nil, fmt.Errorf("httpcache: fetch disabled")
+		}
+		return nil, fmt.Errorf("httpcache: fetch disabled, response not in cache (%s)", cacheName)
+	}
+
+	redacted := req
+	if t.RequestRedactor != nil {
+		redacted = t.RequestRedactor.Redact(req)
+	}
+
+	resp, err := t.Next.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if t.ResponseRedactor != nil {
+		resp = t.ResponseRedactor.RedactResponse(resp)
+	}
+
+	reqbuf, err := httputil.DumpRequest(redacted, true)
+	if err != nil {
+		return nil, err
+	}
+
+	respbuf, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		return nil, err
+	}
+
+	if cacheName != "" {
+		if err := os.WriteFile(cacheName, slices.Concat(reqbuf, respbuf), 0666); err != nil {
+			return nil, fmt.Errorf("httpcache: write cached response: %w", err)
+		}
+	}
+	return resp, nil
+}
+
+// RequestRedactor returns a redacted copy of a request. If fields are modified,
+// they must be copied.
+type RequestRedactor interface {
+	Redact(req *http.Request) *http.Request
+}
+
+// ResponseRedactor redacts a response in-place.
+type ResponseRedactor interface {
+	RedactResponse(r *http.Response) *http.Response
+}
+
+type Redactor struct {
+	req  []func(*http.Request)
+	resp []func(*http.Response)
+}
+
+// RedactRequestHeader redacts headers with the specified name, optionally with
+// the specified number of hashed characters.
+func (r *Redactor) RedactRequestHeader(name string, hashed int) {
+	r.req = append(r.req, func(req *http.Request) {
+		for n, vs := range req.Header {
+			if textproto.CanonicalMIMEHeaderKey(n) == textproto.CanonicalMIMEHeaderKey(name) {
+				for i, v := range vs {
+					req.Header[n][i] = redactHashed(v, hashed)
+				}
+			}
+		}
+	})
+}
+
+// RedactRequestURLParam redacts URL parameters with the specified
+// case-sensitive name, optionally with the specified number of hashed
+// characters.
+func (r *Redactor) RedactRequestURLParam(name string, hashed int) {
+	r.req = append(r.req, func(req *http.Request) {
+		var q strings.Builder
+		fmt.Println(req.URL.RawQuery)
+		for x := range strings.SplitSeq(req.URL.RawQuery, "&") {
+			if q.Len() != 0 {
+				q.WriteByte('&')
+			}
+			k, v, eq := strings.Cut(x, "=")
+			if ku, err := url.QueryUnescape(k); err == nil && ku == name {
+				vu, err := url.QueryUnescape(v)
+				if err == nil {
+					vu = v
+				}
+				v = redactHashed(vu, hashed)
+			}
+			q.WriteString(k)
+			if eq {
+				q.WriteByte('=')
+				q.WriteString(v)
+			}
+		}
+		req.URL.RawQuery = q.String()
+	})
+}
+
+func redactHashed(v string, hashed int) string {
+	if hashed != 0 {
+		h := sha1.Sum([]byte(v))
+		return "redacted-" + hex.EncodeToString(h[:min(hashed, sha1.Size)])
+	}
+	return "redacted"
+}
+
+func (x *Redactor) Redact(r *http.Request) *http.Request {
+	r2 := *r
+	u2 := *r.URL
+	r2.URL = &u2
+	r2.Header = r.Header.Clone()
+	r = &r2
+	for _, fn := range x.req {
+		fn(r)
+	}
+	return r
+}
+
+func (x *Redactor) RedactResponse(r *http.Response) *http.Response {
+	for _, fn := range x.resp {
+		fn(r)
+	}
+	return r
+}

commit 544414941c0a0c33afcb2eb404c19a5d7b3019fb

Fix minor regression in 6843135c30677559adaed049907f792d1ce1ac76
Not hit during normal usage.

scraper/main.go
@@ -124,6 +124,7 @@ func main() {
 				r2.Header = r.Header.Clone()
 				r2.Header.Del("User-Agent")
 				r2.Header.Del("Cookie")
+				r2.Header.Del("X-Scraper-Secret")
 				r = &r2
 				return next.RoundTrip(r)
 			}